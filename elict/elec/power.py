8ecb4bee80488035e8f3bb7d6519f3f23c155a7b
Adam Gibson <adam.gibson@tc4k.net>
2012-07-16 22:32:49 +0800

    added reactive & DC power

--- elict/elec/power.py
+++ elict/elec/power.py
@@ -1,47 +1,107 @@
 import math
 
-def power_3ph_real(Voltage_LL, Current_L, power_factor):
+def power_AC_3ph_real(Voltage_LL, Current_L, power_factor):
     """
-    Calculate 3 phase power (real).
+    Calculate three phase AC power (real).
 
-    :param Voltage_LL: 3 Phase Voltage, Line-Line (V)
+    :param Voltage_LL: Three Phase AC Voltage, Line-Line (V)
+    :type float
     :param Current_L: Line Current (A)
+    :type float
     :param power_factor: Power Factor
+    :type float
     :returns power_real: Real Power (W)
+    :rtype float
     """
     power_real = math.sqrt(3) * Voltage_LL * Current_L * power_factor
     return power_real
 
-def power_3ph_real_alt(Voltage_PN, Current_L, power_factor):
+def power_AC_3ph_real_alt(Voltage_PN, Current_L, power_factor):
     """
-    Calculate 3 phase power (real).
+    Calculate three phase AC power (real).
 
-    :param Voltage_PN: 3 Phase Voltage, Phase-Neutral (V)
+    :param Voltage_PN: Three Phase AC Voltage, Phase-Neutral (V)
+    :type float
     :param Current_L: Line Current (A)
+    :type float
     :param power_factor: Power Factor
+    :type float
     :returns power_real: Real Power (W)
+    :rtype float
     """
     power_real = 3 * Voltage_PN * Current_L * power_factor
     return power_real
 
-def power_3ph_aprnt(Voltage_LL, Current_L):
+def power_AC_3ph_rctv(Voltage_LL, Current_L, power_factor):
     """
-    Calculate 3 phase power (apparent).
+    Calculate three phase AC power (reactive).
 
-    :param Voltage_LL: 3 Phase Voltage, Line-Line (V)
+    :param Voltage_LL: Three Phase AC Voltage, Line-Line (V)
+    :type float
     :param Current_L: Line Current (A)
+    :type float
+    :param power_factor: Power Factor
+    :type float
+    :returns power_apparent: Reactive Power (VAr)
+    :rtype float
+    """
+    power_reactive = math.sqrt(3) * Voltage_LL * Current_L * math.sin(math.acos(power_factor))
+    return power_reactive
+
+def power_AC_3ph_rctv_alt(Voltage_PN, Current_L, power_factor):
+    """
+    Calculate three phase AC power (reactive).
+
+    :param Voltage_PN: Three Phase AC Voltage, Phase-Neutral (V)
+    :type float
+    :param Current_L: Line Current (A)
+    :type float
+    :param power_factor: Power Factor
+    :type float
+    :returns power_apparent: Reactive Power (VAr)
+    :rtype float
+    """
+    power_reactive = 3 * Voltage_PN * Current_L * math.sin(math.acos(power_factor))
+    return power_reactive
+
+def power_AC_3ph_aprnt(Voltage_LL, Current_L):
+    """
+    Calculate three phase AC power (apparent).
+
+    :param Voltage_LL: Three Phase AC Voltage, Line-Line (V)
+    :type float
+    :param Current_L: Line Current (A)
+    :type float
     :returns power_apparent: Apparent Power (VA)
+    :rtype float
     """
     power_apparent = math.sqrt(3) * Voltage_LL * Current_L
     return power_apparent
 
-def power_3ph_aprnt_alt(Voltage_PN, Current_L):
+def power_AC_3ph_aprnt_alt(Voltage_PN, Current_L):
     """
-    Calculate 3 phase power (apparent).
+    Calculate three phase AC power (apparent).
 
-    :param Voltage_PN: 3 Phase Voltage, Phase-Neutral (V)
+    :param Voltage_PN: Three Phase AC Voltage, Phase-Neutral (V)
+    :type float
     :param Current_L: Line Current (A)
+    :type float
     :returns power_apparent: Apparent Power (VA)
+    :rtype float
     """
     power_apparent = 3 * Voltage_PN * Current_L
     return power_apparent
+
+def power_DC(Voltage, Current):
+    """
+    Calculate DC power.
+
+    :param Voltage: DC Voltage (V)
+    :type float
+    :param Current: Line Current (A)
+    :type float
+    :returns power: Power (W)
+    :rtype float
+    """
+    power = Voltage * Current
+    return power
